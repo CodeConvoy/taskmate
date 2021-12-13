ac0b41a1c09439815410a739c606553c4b5ab202
Cooper Saye <coopersaye@gmail.com>
2021-12-13 08:46:02 -0800

    Initialize firebase.

---
+++ util/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCh6kvN2yO0ueV_AxIMFwYwcKJ1UWaT-Zg",
+  authDomain: "taskmate-app.firebaseapp.com",
+  projectId: "taskmate-app",
+  storageBucket: "taskmate-app.appspot.com",
+  messagingSenderId: "339142194238",
+  appId: "1:339142194238:web:9ec7cb967bc7c1c8264d6f"
+};
