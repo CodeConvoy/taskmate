2db3f5f02eb35bddce20251a090864e47c5fc0d8
Cooper Saye <coopersaye@gmail.com>
2021-12-13 10:04:57 -0800

    Create signInWithGoogle function.

---
+++ util/signInWithGoogle.js
@@ -0,0 +1,11 @@
+import Router from 'next/router';
+
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+// opens google sign in popup
+export default async function signInWithGoogle() {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  await signInWithPopup(auth, provider);
+  Router.push('/home');
+}
