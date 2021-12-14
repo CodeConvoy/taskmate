2204f311b6642d7ff29e5f0673494454980275a8
Cooper Saye <coopersaye@gmail.com>
2021-12-13 22:05:22 -0800

    Implement task deletion.

--- components/Task.js
+++ components/Task.js
@@ -1,11 +1,23 @@
+import deleteDoc from 'firebase/firestore';
+
 import styles from '../styles/components/Task.module.css';
 
 export default function Task(props) {
+  const { task, taskRef } = props;
   const { title } = props.task;
+
+  // deletes task in firebase
+  async function deleteTask() {
+    if (!window.confirm('Delete task?')) return;
+    await deleteDoc(taskRef);
+  }
 
   return (
     <div>
       <h1>{title}</h1>
+      <button onClick={deleteTask}>
+        Delete
+      </button>
     </div>
   );
 }
