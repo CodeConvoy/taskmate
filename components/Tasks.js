934230cb81d2758f2e6d121d77c5b3d2b17527e8
Cooper Saye <coopersaye@gmail.com>
2021-12-13 22:07:42 -0800

    Listen for tasks.

--- components/Tasks.js
+++ components/Tasks.js
@@ -1,8 +1,21 @@
 import Task from './Task';
+import Loading from './Loading';
+
+import { getFirestore, collection } from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import styles from '../styles/components/Tasks.module.css';
 
 export default function Tasks() {
+  const db = getFirestore();
+
+  // listen for tasks
+  const tasksRef = collection(db, 'tasks');
+  const [tasks] = useCollectionData(tasksRef, { idField: 'id' });
+
+  // return if loading tasks
+  if (!tasks) return <Loading />;
+
   return (
     <div>
       {
