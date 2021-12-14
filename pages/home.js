3d85db0069161b35eb7aaf2936a85a6b0e5b570c
Cooper Saye <coopersaye@gmail.com>
2021-12-13 22:09:03 -0800

    Route from home page.

--- pages/home.js
+++ pages/home.js
@@ -1,8 +1,21 @@
+import Router from 'next/router';
 import Tasks from '../components/Tasks';
+
+import { useEffect } from 'react';
 
 import styles from '../styles/pages/Home.module.css';
 
-export default function Home() {
+export default function Home(props) {
+  const { authed } = props.authed;
+
+  // route away if not authed
+  useEffect(() => {
+    if (authed === false) Router.push('/');
+  }, [authed]);
+
+  // return loading if not authed
+  if (!authed) return <Loading />;
+
   return (
     <div>
       <Tasks />
