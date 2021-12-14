450a17e9ba0b0f746dc914db160de2ecb007e5b4
Cooper Saye <coopersaye@gmail.com>
2021-12-13 22:10:01 -0800

    Implement task creation.

--- components/Tasks.js
+++ components/Tasks.js
@@ -1,7 +1,8 @@
 import Task from './Task';
 import Loading from './Loading';
 
-import { getFirestore, collection } from 'firebase/firestore';
+import { useState } from 'react';
+import { getFirestore, collection, addDoc, doc } from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import styles from '../styles/components/Tasks.module.css';
@@ -9,9 +10,16 @@ import styles from '../styles/components/Tasks.module.css';
 export default function Tasks() {
   const db = getFirestore();
 
+  const [title, setTitle] = useState('');
+
   // listen for tasks
   const tasksRef = collection(db, 'tasks');
   const [tasks] = useCollectionData(tasksRef, { idField: 'id' });
+
+  // adds new task in firebase
+  async function addTask() {
+    await addDoc(tasksRef, { title });
+  }
 
   // return if loading tasks
   if (!tasks) return <Loading />;
@@ -20,9 +28,24 @@ export default function Tasks() {
     <div>
       {
         tasks.map(task =>
-          <Task task={task} key={task.id} />
+          <Task
+            task={task}
+            taskRef={doc(tasksRef, task.id)}
+            key={task.id}
+          />
         )
       }
+      <form onSubmit={e => {
+        e.preventDefault();
+        addTask();
+      }}>
+        <input
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          required
+        />
+        <button>New Task</button>
+      </form>
     </div>
   );
 }
