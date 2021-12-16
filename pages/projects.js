6348078a79d09af16925133d3bfeceb4a6a40593
Cooper Saye <coopersaye@gmail.com>
2021-12-15 23:02:29 -0800

    Implement project creation.

--- pages/projects.js
+++ pages/projects.js
@@ -1,17 +1,26 @@
 import Loading from '../components/Loading';
 import Project from '../components/Project';
 
-import { getFirestore, collection } from 'firebase/firestore';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useState } from 'react';
 
 import styles from '../styles/pages/Projects.module.css';
 
 export default function Projects() {
   const db = getFirestore();
 
+  const [title, setTitle] = useState('');
+
   // listen for projects
   const projectsRef = collection(db, 'projects');
   const [projects] = useCollectionData(projectsRef, { idField: 'id' });
+
+  // adds new project in firebase
+  async function addProject() {
+    setTitle('');
+    await addDoc(projectsRef, { title });
+  }
 
   return (
     <div>
@@ -23,6 +32,20 @@ export default function Projects() {
           <Project project={project} key={project.id} />
         )
       }
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          addProject();
+        }}
+      >
+        <input
+          placeholder="Title"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          required
+        />
+        <button>New Task</button>
+      </form>
     </div>
   );
 }
