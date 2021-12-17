c60759c22d6c757906ae82180f836a8fc078b28a
Cooper Saye <coopersaye@gmail.com>
2021-12-16 22:46:11 -0800

    Listen for project tasks.

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,10 +1,22 @@
 import { useRouter } from 'next/router';
+import {
+  getFirestore, collection, doc, query, orderBy
+} from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import styles from '../../styles/pages/Project.module.css';
 
 export default function Project() {
+  const db = getFirestore();
+
+  // get project id
   const router = useRouter();
   const { id } = router.query;
+
+  // listen for tasks
+  const tasksRef = collection(db, 'projects', id ?? '~', 'tasks');
+  const tasksQuery = query(tasksRef, orderBy('date', 'desc'));
+  const [tasks] = useCollectionData(tasksQuery, { idField: 'id' });
 
   return (
     <div>
