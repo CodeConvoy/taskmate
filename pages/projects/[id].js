a37e3ee19836b2430d5007f9ffca7339ea77744f
Cooper Saye <coopersaye@gmail.com>
2021-12-17 23:04:56 -0800

    Create drag drop context.

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -8,6 +8,7 @@ import {
   getFirestore, collection, doc, query, orderBy, addDoc
 } from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import styles from '../../styles/pages/Project.module.css';
 
@@ -39,19 +40,15 @@ export default function Project() {
   return (
     <div className={styles.container}>
       <h1>Project {id}</h1>
-      <div className={styles.tasks}>
-        {
-          !tasks ?
-          <Loading /> :
-          tasks.map(task =>
-            <Task
-              task={task}
-              taskRef={doc(tasksRef, task.id)}
-              key={task.id}
-            />
-          )
-        }
-      </div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="tasks">
+          {
+            (provided, snapshot) =>
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+            </div>
+          }
+        </Droppable>
+      </DragDropContext>
       <form
         className={styles.form}
         onSubmit={e => {
